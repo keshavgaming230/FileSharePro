8cc2d386859534c5e0a3249e480907ae76c693b7
KeshavGamingTech <283448404+keshavgaming230@users.noreply.github.com>
2026-05-10 23:02:01 +0400

    Add files via upload

--- shareit_clone.py
+++ shareit_clone.py
@@ -957,4 +957,5 @@ def main():
 
 if __name__ == '__main__':
     create_templates()
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    port = int(os.environ.get("PORT", DEFAULT_PORT))
+    app.run(host='0.0.0.0', port=port)
